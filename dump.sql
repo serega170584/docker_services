22a3bec43cec20e95d998c2b91fc2ada72a03d4c
krivobokov <krivobokov_sv@magnit.ru>
2024-01-09 14:58:36 +0300

    dump fix

--- dump.sql
+++ dump.sql
@@ -3,11 +3,11 @@ CREATE TABLE product_user (
                               login VARCHAR(255) NOT NULL UNIQUE,
                               first_name VARCHAR(255) NOT NULL,
                               second_name VARCHAR(255) NOT NULL,
-                              password CHAR(75) NOT NULL,
-                              token CHAR(512) NOT NULL
+                              password VARCHAR(75) NOT NULL,
+                              token VARCHAR(512) NOT NULL
 );
 
 drop table product_user;
 
 INSERT INTO product_user(login, first_name, second_name, password, token)
-VALUES('asdasdasd', 'Test', 'Test', '$argon2id$asdasdasd', '');
+VALUES('test', 'Test', 'Test', 'adasd', '12345');
